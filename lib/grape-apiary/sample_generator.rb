adf0a27775a26dd3985c6677821361faf07bb21a
Timofey Martynov <feymartynov@gmail.com>
2015-04-15 20:03:24 +0200

    fix nested-parameters

--- lib/grape-apiary/sample_generator.rb
+++ lib/grape-apiary/sample_generator.rb
@@ -1,55 +1,64 @@
 module GrapeApiary
   class SampleGenerator
-    attr_reader :resource, :root
+    attr_reader :resource
 
     delegate :unique_params, to: :resource
 
     def initialize(resource)
       @resource = resource
-      @root     = resource.key.singularize
     end
 
-    def sample(id = false)
-      array = resource.unique_params.map do |param|
-        next if param.name == root
+    def sample
+      @sample ||= begin
+        resource.unique_params.inject({}) do |result, param|
+          parsed_name = param.name.scan(/\[([^\]]+)\]/).flatten
+          name = parsed_name.pop || param.name
 
-        [param.name, param.example]
+          node = parsed_name.inject(result) do |n, k|
+            n[k] ||= begin
+              parent_name = param.name.match(/(.+)\[.+\]/)[1]
+              parent = resource.unique_params.find { |p| p.name == parent_name }
+              parent.type == 'Array' ? [{}] : {}
+            end
+
+            n[k].is_a?(Array) ? n[k].first : n[k]
+          end
+
+          node[name] = param.example
+          result
+        end
       end
-
-      hash = Hash[array.compact]
-
-      hash = hash.reverse_merge(id: Config.generate_id) if id
-      hash = { root => hash } if Config.include_root
-
-      hash
     end
 
     def request
-      hash = sample
-
-      return unless hash.present?
-
-      # format json spaces for blueprint markdown
-      JSON.pretty_generate(hash)
-        .gsub('{', (' ' * 14) + '{')
-        .gsub('}', (' ' * 14) + '}')
-        .gsub(/\ {2}\"/, (' ' * 16) + '"')
+      pretty_json(sample) if sample.present?
     end
 
-    def response(list = false)
-      hash = sample(true)
+    def response
+      return unless sample.present?
 
-      return unless hash.present?
+      hash = sample.reverse_merge(id: GrapeApiary::Config.generate_id)
+      # sample = [sample] if list?(route)
 
-      hash = [hash] if list
+      pretty_json(hash)
+    end
 
+    private
+
+    def pretty_json(hash)
       # format json spaces for blueprint markdown
-      JSON.pretty_generate(hash)
-        .gsub('[', (' ' * 12) + '[')
-        .gsub(']', (' ' * 12) + ']')
-        .gsub('{', (' ' * 14) + '{')
-        .gsub('}', (' ' * 14) + '}')
-        .gsub(/\ {2}\"/, (' ' * 16) + '"')
+      indent(JSON.pretty_generate(hash), 2, "\t")
+    end
+
+    def indent(string, count, char = ' ')
+      string.gsub(/([^\n]*)(\n|$)/) do |match|
+        last_iteration = ($1 == '' && $2 == '')
+        line = ''
+        line << (char * count) unless last_iteration
+        line << $1
+        line << $2
+        line
+      end
     end
   end
 end
